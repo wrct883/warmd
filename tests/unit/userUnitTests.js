f94115d47f60c925b49fdce50f20ecc9d46b7b55
Marcus Todd <malexander.todd@gmail.com>
2017-12-09 18:44:13 -0500

    test(userUnitTests): add before hook to connect to database

--- tests/unit/userUnitTests.js
+++ tests/unit/userUnitTests.js
@@ -1,9 +1,27 @@
 'use strict';
 
 var expect = require('chai').expect,
-    User = require('../../app/schema/userModel');
+    mongoose = require('mongoose');
+
+// Mongoose models
+var User = require('../../app/schema/userModel');
 
 describe('The User Model', function() {
+  before(function(done) {
+    mongoose.connect('mongodb://localhost/warmd', {useMongoClient: true});
+    mongoose.Promise = global.Promise;
+
+    // Set up event handlers
+    var db = mongoose.connection;
+    db.on('error', function(err) {
+      done(err);
+    });
+    db.once('open', function() {
+      console.log('Connected to MongoDB instance');
+      done();
+    });
+  });
+
   it('should not validate new users with insufficient information', function(done) {
     var newUser = new User({
       email: 'newuser@example.com'
