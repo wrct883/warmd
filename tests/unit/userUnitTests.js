7a5c92af5f63939b113a026e1cf1c35c7929a4bd
Marcus Todd <malexander.todd@gmail.com>
2017-12-11 18:13:00 -0500

    test(*): make mongo tests main ones that run

--- tests/unit/userUnitTests.js
+++ tests/unit/userUnitTests.js
@@ -1,25 +1,13 @@
 'use strict';
 
 var expect = require('chai').expect,
-    mongoose = require('mongoose');
-
-// Mongoose models
-var User = require('../../app/schema/userModel');
+    User = require('../../app/schema/userModel');
 
 describe('The User Model', function() {
   before(function(done) {
-    mongoose.connect('mongodb://localhost/warmd', {useMongoClient: true});
-    mongoose.Promise = global.Promise;
-
-    // Set up event handlers
-    var db = mongoose.connection;
-    db.on('error', function(err) {
-      done(err);
-    });
-    db.once('open', function() {
-      console.log('Connected to MongoDB instance');
-      done();
-    });
+    // Start warmd server
+    require('../../server');
+    done();
   });
 
   it('should not validate new users with insufficient information', function(done) {
