3a69ac86753683cd351403f3b29dbb644222b7b8
Marcus Todd <malexander.todd@gmail.com>
2017-12-11 02:48:59 -0500

    test(userIntegrationTests): add before/after hooks and email /exists test

--- tests/integration/userIntegrationTests.js
+++ tests/integration/userIntegrationTests.js
@@ -2,20 +2,80 @@
 
 var expect = require('chai').expect,
     request = require('supertest'),
-    app = require('../../server');
+    app = require('../../server'),
+    User = require('../../app/schema/userModel');
 
 describe('The Users controller', function() {
-  it('should check if a User exists given their username', function(done) {
-    request.agent(app)
-      .post('/users/exists')
-      .send({
-        username: 'matodd'
+  before(function(done) {
+    var testUser1 = new User({
+      username: 'test1',
+      password: 'secret1',
+      email: 'test1@example.com'
+    });
+
+    var testUser2 = new User({
+      username: 'test2',
+      password: 'secret2',
+      email: 'test2@example.com'
+    });
+
+    var testUser3 = new User({
+      username: 'test3',
+      password: 'secret3',
+      email: 'test3@example.com'
+    });
+
+    testUser1.save()
+      .then(function() {
+        return testUser2.save();
       })
-      .end(function(err, res) {
-        expect(err).to.be.null;
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.have.property('exists', false);
+      .then(function() {
+        return testUser3.save();
+      })
+      .then(function() {
         done();
+      });
+  });
+
+  describe('GET /users/exists', function() {
+    it('should check if a User exists given their username', function(done) {
+      request.agent(app)
+        .post('/users/exists')
+        .send({
+          email: 'test1@example.com'
+        })
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('exists', true);
+          done();
+        });
+    });
+
+    it('should check if a User exists given their email', function(done) {
+      request.agent(app)
+        .post('/users/exists')
+        .send({
+          email: 'test1@example.com'
+        })
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('exists', true);
+          done();
+        });
+    });
+  });
+
+  after(function(done) {
+    User
+      .remove({})
+      .then(function() {
+        console.log('Users collection dropped');
+        done();
+      })
+      .catch(function(err) {
+        done(err);
       });
   });
 });
