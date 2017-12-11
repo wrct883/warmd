a43acc8cc67082143b9af36fe7a953d66222bec2
Marcus Todd <malexander.todd@gmail.com>
2017-12-10 19:54:22 -0500

    test(userUnitTests): add tests for updating and deleting Users

--- tests/unit/userUnitTests.js
+++ tests/unit/userUnitTests.js
@@ -73,6 +73,59 @@ describe('The User Model', function() {
       });
   });
 
+  it('should update that new User', function(done) {
+    var update = {
+      first_name: 'User'
+    };
+
+    // We initally expect the User to not have a first name
+    User
+      .findOne({username: 'user'})
+      .then(function(user) {
+        expect(user).to.have.property('username', 'user');
+        expect(user.first_name).to.be.undefined;
+      })
+      .catch(function(err) {
+        done(err);
+      });
+
+    // Now let's add one
+    User
+      .findOneAndUpdate({username: 'user'}, update, {new: true})
+      .then(function(updatedUser) {
+        expect(updatedUser).to.have.property('first_name', 'User');
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  it('should delete a user', function(done) {
+    User
+      .count({})
+      .then(function(count) {
+        expect(count).to.equal(1);
+      })
+      .catch(function(err) {
+        done(err);
+      });
+
+    User
+      .findOneAndRemove({username: 'user'})
+      .then(function(removedUser) {
+        expect(removedUser).to.have.property('username', 'user');
+        User.count({}, function(err, count) {
+          if (err) done(err);
+          expect(count).to.equal(0);
+          done();
+        });
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
   after(function(done) {
     // Drop Users collection
     User.remove({}, function(err) {
