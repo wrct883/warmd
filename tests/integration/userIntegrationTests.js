d5dc3f5ad04390071926802f7b4eefd7f3a7c977
Marcus Todd <malexander.todd@gmail.com>
2017-12-11 16:49:53 -0500

    test(userIntegrationTests): add tests for /users/:user

--- tests/integration/userIntegrationTests.js
+++ tests/integration/userIntegrationTests.js
@@ -101,6 +101,49 @@ describe('The Users controller', function() {
     });
   });
 
+  describe('/users/:user', function() {
+    it('should retrive a User with a GET request', function(done) {
+      request.agent(app)
+        .get('/users/test1')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('username', 'test1');
+          expect(res.body).to.have.property('email', 'test1@example.com');
+          expect(res.body).to.not.have.property('password');
+          done();
+        });
+    });
+
+    it('should modify a User with a PUT request', function(done) {
+      request.agent(app)
+        .put('/users/test1')
+        .send({
+          first_name: 'Test',
+          last_name: 'One'
+        })
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('first_name', 'Test');
+          expect(res.body).to.have.property('last_name', 'One');
+          expect(res.body).to.not.have.property('password');
+          done();
+        });
+    });
+
+    it('should delete a User with a DELETE request', function(done) {
+      request.agent(app)
+        .delete('/users/test4')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('removedUser', 'test4');
+          done();
+        });
+    });
+  });
+
   after(function(done) {
     User.remove({})
       .then(function() {
