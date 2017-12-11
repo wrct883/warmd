8f1a0631b11dce4893d77d0781aea4300fb0907f
Marcus Todd <malexander.todd@gmail.com>
2017-12-10 22:36:14 -0500

    feat(userController): add endpoints for updating and loading users

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -5,6 +5,7 @@ var checkit = require('checkit'),
     User = mongoose.model('User');
 
 module.exports = {
+  // Create a new User
   create: function(req, res) {
     var newUser = new User(req.body);
     newUser.save(function(err, user) {
@@ -15,10 +16,40 @@ module.exports = {
     });
   },
 
+  // Load a User from the database
+  load: function(req, res, next, username) {
+    User.findOne({username: username})
+      .then(function(user) {
+        // Can't do req.user, interferes with Passport
+        req.userData = user;
+        next();
+      })
+      .catch(function(err) {
+        next(err);
+      });
+  },
+
+  // Display a User that was loaded
+  show: function(req, res) {
+    res.json(req.userData.toJSON());
+  },
+
+  // Update a User
+  update: function(req, res) {
+    User
+      .findOneAndUpdate(req.userData.username, req.body, {new: true})
+      .then(function(updatedUser) {
+        res.json(updatedUser);
+      })
+      .catch(function(err) {
+        res.send(err);
+      });
+  },
+
   exists: function(req, res) {
     if (!req.body.username && !req.body.email) {
       res.json(400, {
-        error: '/exists must contain either a username, an email, or both'
+        error: 'GET /exists must contain either a username, an email, or both'
       });
     }
 
