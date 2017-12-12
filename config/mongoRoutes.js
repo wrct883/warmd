6555674191df6674b14794f6a8121b20f0abd0be
Marcus Todd <malexander.todd@gmail.com>
2017-12-12 15:25:59 -0500

    fix(mongoRoutes): replace login and logout with one auth route

--- config/mongoRoutes.js
+++ config/mongoRoutes.js
@@ -4,30 +4,17 @@ var userController = require('../app/controllers/userController'),
     express = require('express');
 
 module.exports = function(app, config, passport) {
-  // Login and logout
-  app.route('/login').get(function(req, res, next) {
-    if (req.user) {
-      res.redirect('/');
-    } else next();
-  }, userController.login);
-
-  app.route('/logout').get(userController.logout);
-
   // Session init
-  app.route('/users/session')
-    .get(function(req, res) {
-      res.redirect('/');
-    })
-    .post(passport.authenticate('local', {
-      successRedirect: '/app',
-      failureRedirect: '/login?success=false'
-    }));
+  app.post('/auth',
+    passport.authenticate('local'), function(req, res) {
+      res.json({ok: 1});
+    });
 
   // User routes
   var userRouter = new express.Router()
     .param('user', userController.load)
     .post('/exists', userController.exists)
-    .post('/new', userController.create)
+    .post('/new', userController.isAuthed, userController.create)
     .get('/:user', userController.show)
     .put('/:user', userController.update)
     .delete('/:user', userController.delete);
