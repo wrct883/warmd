5bf896250a9b32e06980d89c1f7a60c59b4bb711
Marcus Todd <malexander.todd@gmail.com>
2017-12-29 02:36:52 -0500

    feat(*): init album model/controller

---
+++ app/schema/albumModel.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    shortid = require('shortid');
+
+var Schema = mongoose.Schema;
+var NestedArtistSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true
+  }
+});
+
+var AlbumSchema = new Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  artist: {
+    type: [NestedArtistSchema],
+    required: true
+  },
+  label: {
+    type: String,
+    trim: true
+  },
+  format: {
+    type: String,
+    enum: ['LP', '12"', '10"', 'EP', '7"', 'CD', 'Cassette', 'Digital']
+  },
+  year: {
+    type: Number
+  },
+  is_comp: {
+    type: Boolean
+  },
+  location: {
+    type: String,
+    enum: ['Bin', 'TBR', 'N&WC', 'OOB', 'Missing', 'NIB']
+  },
+  library_add_date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  bin_add_date: {
+    type: Date
+  },
+  bin_removal_date: {
+    type: Date
+  },
+  genre: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Album', AlbumSchema);
