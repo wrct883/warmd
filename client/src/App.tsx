32c6a336d62f4490b8e6dc7de1e2ae710af64d81
Marcus Todd <malexander.todd@gmail.com>
2020-01-09 13:06:07 -0500

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './static/images/wrct_logo.png';
 import './static/sass/App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to WRCT!</h1>
-        <Navbar bg="dark" />
-      </header>
-      <p className="App-intro">
-        TODO: Build the whole site
-      </p>
+      <Navbar className="App-navbar">
+        <Navbar.Brand>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to WRCT!</h1>
+          </header>
+        </Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Item>
+            <Nav.Link className="App-nav-link" href="">My Account</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="App-nav-link" href="">Programs</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="App-nav-link" href="">Artists</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="App-nav-link" href="">Albums</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+      <p className="App-intro">TODO: Build the whole site</p>
     </div>
   );
 };
